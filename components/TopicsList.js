0f1543646d41acf7a205a7ffba7cbabd6499b636
Vikash <vikash2000158@gmail.com>
2023-08-03 16:37:24 +0530

    commit submit

--- components/TopicsList.js
+++ components/TopicsList.js
@@ -3,33 +3,40 @@ import RemoveBtn from './RemoveBtn'
 import Link from 'next/link'
 import { HiPencilAlt } from 'react-icons/hi';
 
-const getTopics= async () =>{
+const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/topics", {cache:"no-store"});
+    const res = await fetch("http://localhost:3000/api/topics", { cache: "no-store" });
 
-    if(!res.ok){
+    if (!res.ok) {
       throw new Error("Failed to fetch topics");
     }
     return res.json();
   }
-   catch (error) {
-    console.log("Error loading Topics",error)
+  catch (error) {
+    console.log("Error loading Topics", error)
   }
 }
 
 const TopicsList = async () => {
-  
-   const {topics}=await getTopics();
-    
-    
+
+ 
+    let crudbd = await getTopics();
+    console.log(crudbd)
+        
+
+
+ 
+
+
+
 
 
   return (
     <>
       {
-        topics.map((value) => {
+        crudbd.topics.map((value, index) => {
           return (
-            <div className='my-2' key={value._id}>
+            <div className='my-2' key={index}>
               <div className='flex justify-between items-start border border-blue-400 p-4 rounded-md'>
                 <div>
                   <h1>{value.title} </h1>
