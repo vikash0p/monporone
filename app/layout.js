b3c8775728767fc00fff5aae79066223204ddbe2
Vikash <vikash2000158@gmail.com>
2023-08-03 16:02:46 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
 import Navbar from '@/components/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'CRUD',
@@ -13,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <body className={inter.className}>
+    <body>
       <div className="max-w-3xl mx-auto p-4">
         <Navbar />
         <div className="mt-8">{children}</div>
